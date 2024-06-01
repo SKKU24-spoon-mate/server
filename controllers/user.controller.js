0cdc23060653eb10958701595a9284ea6c4b65e3
smori <summer2788@g.skku.edu>
2024-06-01 23:56:16 +0900

    modify user api spec

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,15 +1,22 @@
 const User = require('../models/user.model');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 exports.register = async (req, res) => {
     try {
-        const { username, password } = req.body;
-        const salt = await bcrypt.genSalt(10);
-        const hashedPassword = await bcrypt.hash(password, salt);
+        const { id, pw, nickname, sex, age } = req.body;
 
-        const newUser = new User({ username, password: hashedPassword });
+        // Validate input
+        if (!id || !pw || !nickname || !sex || !age) {
+            return res.status(400).json({ error: 'Missing required fields or invalid data' });
+        }
+
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(pw, salt);
+
+        const newUser = new User({ id, password: hashedPassword, nickname, sex, age });
         await newUser.save();
-        res.status(201).send('User registered');
+        res.status(200).json({ message: 'Registration successful' });
     } catch (error) {
         res.status(500).send(error.message);
     }
@@ -17,14 +24,14 @@ exports.register = async (req, res) => {
 
 exports.login = async (req, res) => {
     try {
-        const { username, password } = req.body;
-        const user = await User.findOne({ username });
+        const { id, pw } = req.body;
+        const user = await User.findOne({ id });
 
-        if (user && await bcrypt.compare(password, user.password)) {
-            req.session.userId = user._id;
-            res.send('User logged in');
+        if (user && await bcrypt.compare(pw, user.password)) {
+            const token = jwt.sign({ userId: user._id }, 'your_jwt_secret_key', { expiresIn: '1h' });
+            res.status(200).json({ message: 'Login successful', token, userId: user._id });
         } else {
-            res.status(400).send('Invalid credentials');
+            res.status(401).json({ error: 'Invalid credentials' });
         }
     } catch (error) {
         res.status(500).send(error.message);
--- models/user.model.js
+++ models/user.model.js
@@ -1,13 +1,14 @@
 const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
-    username: { type: String, required: true, unique: true },
-    password: { type: String, required: true }
-},
-    {
-        timestamps: true,
-    }
-);
+    id: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    nickname: { type: String, required: true },
+    sex: { type: String, required: true, enum: ['Male', 'Female', 'Other'] },
+    age: { type: Number, required: true }
+}, {
+    timestamps: true,
+});
 
 // We have to make it singular because mongoose will automatically look for the plural, lowercased version of our model name.
 const User = mongoose.model('User', UserSchema);
