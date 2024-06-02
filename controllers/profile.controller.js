935b8dbd56491c32a9cf2b51c490f7af8a95be59
smori <summer2788@g.skku.edu>
2024-06-03 01:01:16 +0900

    getprofile api update

--- controllers/profile.controller.js
+++ controllers/profile.controller.js
@@ -3,7 +3,7 @@ const User = require('../models/user.model');
 // Get User Profile
 exports.getProfile = async (req, res) => {
     try {
-        const userId = req.params.user_id || req.user.id; // Use user_id if provided, otherwise use authenticated user's ID
+        const userId = req.params.user_id || req.body; // Use user_id if provided, otherwise use authenticated user's ID
 
         const user = await User.findById(userId);
 
